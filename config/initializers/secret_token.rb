134ab9ab321fdfb5d07963324c030bd158c415f9
albertomanso <manso.requejo.alberto@hotmail.com>
2012-11-25 17:44:36 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CuartaApp::Application.config.secret_token = '64c804ffa0c08791ffae3d71be41e2aaa52fd1cb842809afa512d673c5c3a1221647fcede9e9f19451bd4e0a21b3f3b09d61dde72ad0ee184405e9daf882d099'
